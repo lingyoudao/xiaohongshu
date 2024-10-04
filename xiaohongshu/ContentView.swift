c68ad9911307d8ddd9607b944cdab2b27db8fbe2
lingyoudao <lingyoudao@hotmail.com>
2024-10-04 08:06:25 +0800

    Initial Commit

---
+++ xiaohongshu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xiaohongshu
+//
+//  Created by 凌有道 on 2024/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xiaohongshu/xiaohongshuApp.swift
@@ -0,0 +1,17 @@
+//
+//  xiaohongshuApp.swift
+//  xiaohongshu
+//
+//  Created by 凌有道 on 2024/10/4.
+//
+
+import SwiftUI
+
+@main
+struct xiaohongshuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
